04879d16b9966681648bf8b96aada2cf1863eda9
Dmytro <vdude112358@gmail.com>
2022-08-14 11:11:07 +0300

    Add new route and StartForm

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for, session
 
 from config import Config
-
+from forms.StartForm import StartForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return 'OK', 200
+    form = StartForm()
+    if form.validate_on_submit():
+        session['username'] = form.username.data
+        return redirect(url_for('minefield'))
+
+    return render_template('main.html', form=form)
+
+
+@app.route('/minefield', methods=['GET', 'POST'])
+def minefield():
+    return render_template('minefield.html', name=session['username'])
 
 
 if __name__ == '__main__':
